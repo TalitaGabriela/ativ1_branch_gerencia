cbf8687df04e79245deacb71ee3aae27f9a7718b
TalitaGabriela <talitagabriela1216@gmail.com>
2024-11-13 01:52:41 -0400

    commit

--- app.js
+++ app.js
@@ -11,6 +11,36 @@ app.use(express.static(path.join(__dirname, 'public')));
 // Middleware para fazer o parse do corpo das requisições
 app.use(express.urlencoded({ extended: true }));
 
+// Rota de Cadastro de Usuários
+app.get('/cadastro', (req, res) => {
+  res.render('cadastro');
+});
+
+app.post('/cadastro', (req, res) => {
+  const { nome, email, senha } = req.body;
+  usuarios.push({ nome, email, senha });
+  res.redirect('/login');
+});
+
+// Rota de Login de Usuários
+app.get('/login', (req, res) => {
+  res.render('login');
+});
+
+app.post('/login', (req, res) => {
+  const { email, senha } = req.body;
+  const usuario = usuarios.find(user => user.email === email && user.senha === senha);
+  if (usuario) {
+    res.redirect('/produtos');
+  } else {
+    res.send('Credenciais incorretas!');
+  }
+});
+
+app.get('/produtos', (req, res) => {
+  res.render('produtos', { produtos });
+});
+
 // Porta do servidor
 const port = 3000;
 app.listen(port, () => {
